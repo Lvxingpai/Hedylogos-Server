bffd292a2ebdc872bd1a3e84ff7eacf15b7d2fff
Zephyre <haizi.zh@gmail.com>
2015-04-29 15:12:42 +0800

    MOD：将部分方法异步化

--- app/controllers/GroupCtrl.scala
+++ app/controllers/GroupCtrl.scala
@@ -18,59 +18,60 @@ object GroupCtrl extends Controller {
       val groupType = (jsonNode \ "groupType").asOpt[String].get
       val isPublic = (jsonNode \ "isPublic").asOpt[Boolean].get
 
-      val group = Group.createGroup(uid, name, groupType, isPublic)
-      group.map(msg => {
+      for {
+        group <- Group.createGroup(uid, name, groupType, isPublic)
+      } yield {
         val result = JsObject(Seq(
-          "groupId" -> JsNumber(msg.getGroupId.toLong),
-          "name" -> JsString(msg.getName),
-          "creator" -> JsNumber(msg.getCreator.toLong),
-          "groupType" -> JsString(msg.getType),
-          "isPublic" -> JsBoolean(msg.getVisible)
+          "groupId" -> JsNumber(group.getGroupId.toLong),
+          "name" -> JsString(group.getName),
+          "creator" -> JsNumber(group.getCreator.toLong),
+          "groupType" -> JsString(group.getType),
+          "isPublic" -> JsBoolean(group.getVisible)
         ))
         Helpers.JsonResponse(data = Some(result))
-      })
+      }
     }
   }
 
-//  def modifyGroup() = Action.async {
-//    request => {
-//      val uid = request.headers.get("UserId").get.toLong
-//      val jsonNode = request.body.asJson.get
-//      val name = (jsonNode \ "name").asOpt[String].get
-//      val desc = (jsonNode \ "desc").asOpt[String].get
-//      // 添加成员-addMember，删除成员-delMember
-//      val action = (jsonNode \ "action").asOpt[String]
-//      val participants = (jsonNode \ "participants").asOpt[Array[Int]].get
-//      val admin = (jsonNode \ "participants").asOpt[Array[Int]].get
-//      val maxUsers = (jsonNode \ "maxUsers").asOpt[Int].get
-//      // 加入公开群是否需要批准, 默认值是true,此属性为可选的
-//      val approval = (jsonNode \ "approval").asOpt[Boolean].get
-//      // 群组成员最大数(包括群主), 值为数值类型,默认值200,此属性为可选的
-//      val public = (jsonNode \ "public").asOpt[Boolean].get
-//
-//      //val group = Group.createGroup(uid, name)
-//
-//      //val jsonNode = request.body.asJson.get
-//      val senderId = (jsonNode \ "sender").asOpt[Long].get
-//      val recvId = (jsonNode \ "receiver").asOpt[Long]
-//      val cid = (jsonNode \ "conversation").asOpt[String]
-//      val msgType = (jsonNode \ "msgType").asOpt[Int].get
-//      val contents = (jsonNode \ "contents").asOpt[String].get
-//
-//      val futureMsg = if (cid.nonEmpty)
-//        Chat.sendMessage(msgType, contents, new ObjectId(cid.get), senderId)
-//      else
-//        Chat.sendMessage(msgType, contents, recvId.get, senderId)
-//
-//      futureMsg.map(msg => {
-//        val result = JsObject(Seq(
-//          "conversation" -> JsString(msg.getConversation.toString),
-//          "msgId" -> JsNumber(msg.getMsgId.toLong),
-//          "timestamp" -> JsNumber(msg.getTimestamp.toLong)
-//        ))
-//        Helpers.JsonResponse(data = Some(result))
-//      })
-//    }
-//  }
+  //  def modifyGroup() = Action.async {
+  //    request => {
+  //      val uid = request.headers.get("UserId").get.toLong
+  //      val jsonNode = request.body.asJson.get
+  //      val name = (jsonNode \ "name").asOpt[String].get
+  //      val desc = (jsonNode \ "desc").asOpt[String].get
+  //      // 添加成员-addMember，删除成员-delMember
+  //      val action = (jsonNode \ "action").asOpt[String]
+  //      val participants = (jsonNode \ "participants").asOpt[Array[Int]].get
+  //      val admin = (jsonNode \ "participants").asOpt[Array[Int]].get
+  //      val maxUsers = (jsonNode \ "maxUsers").asOpt[Int].get
+  //      // 加入公开群是否需要批准, 默认值是true,此属性为可选的
+  //      val approval = (jsonNode \ "approval").asOpt[Boolean].get
+  //      // 群组成员最大数(包括群主), 值为数值类型,默认值200,此属性为可选的
+  //      val public = (jsonNode \ "public").asOpt[Boolean].get
+  //
+  //      //val group = Group.createGroup(uid, name)
+  //
+  //      //val jsonNode = request.body.asJson.get
+  //      val senderId = (jsonNode \ "sender").asOpt[Long].get
+  //      val recvId = (jsonNode \ "receiver").asOpt[Long]
+  //      val cid = (jsonNode \ "conversation").asOpt[String]
+  //      val msgType = (jsonNode \ "msgType").asOpt[Int].get
+  //      val contents = (jsonNode \ "contents").asOpt[String].get
+  //
+  //      val futureMsg = if (cid.nonEmpty)
+  //        Chat.sendMessage(msgType, contents, new ObjectId(cid.get), senderId)
+  //      else
+  //        Chat.sendMessage(msgType, contents, recvId.get, senderId)
+  //
+  //      futureMsg.map(msg => {
+  //        val result = JsObject(Seq(
+  //          "conversation" -> JsString(msg.getConversation.toString),
+  //          "msgId" -> JsNumber(msg.getMsgId.toLong),
+  //          "timestamp" -> JsNumber(msg.getTimestamp.toLong)
+  //        ))
+  //        Helpers.JsonResponse(data = Some(result))
+  //      })
+  //    }
+  //  }
 
 }
--- app/core/Group.scala
+++ app/core/Group.scala
@@ -4,7 +4,6 @@ import core.connector.MorphiaFactory
 import models._
 import org.mongodb.morphia.query.{Query, UpdateOperations}
 import play.api.libs.concurrent.Execution.Implicits.defaultContext
-import scala.collection.JavaConversions._
 
 import scala.concurrent.Future
 
@@ -19,20 +18,27 @@ object Group {
    * 通过id获得conversation信息
    */
   def createGroup(creator: Long, name: String, groupType: String, isPublic: Boolean): Future[Group] = {
-    Future {
-      val gId = populateUserId
-      val c = models.Group.create(creator, gId, name, groupType, isPublic)
+
+    val futureGid = populateUserId
+
+    // comprehension
+    for {
+      gid <- futureGid
+    } yield {
+      val c = models.Group.create(creator, scala.Long.box(gid), name, groupType, isPublic)
       ds.save[Group](c)
       c
     }
   }
 
-  def populateUserId: Long = {
-    val query: Query[Sequence] = miscDs.createQuery(classOf[Sequence])
-    query.field("column").equal(Sequence.GROUPID)
-    val ops: UpdateOperations[Sequence] = miscDs.createUpdateOperations(classOf[Sequence]).inc("count")
-    val ret: Sequence = miscDs.findAndModify(query, ops)
-    ret.count
+  def populateUserId: Future[Long] = {
+    Future {
+      val query: Query[Sequence] = miscDs.createQuery(classOf[Sequence])
+      query.field("column").equal(Sequence.GROUPID)
+      val ops: UpdateOperations[Sequence] = miscDs.createUpdateOperations(classOf[Sequence]).inc("count")
+      val ret: Sequence = miscDs.findAndModify(query, ops)
+      ret.count
+    }
   }
 
 }
