c70ae778ccfc262640f7579092397438c9c74117
Zephyre <haizi.zh@gmail.com>
2015-05-05 15:35:45 +0800

    ADD: 发送音频消息时，附带时长信息

--- app/controllers/MiscCtrl.scala
+++ app/controllers/MiscCtrl.scala
@@ -4,14 +4,29 @@ import controllers.ChatCtrl.MessageInfo
 import core.GlobalConfig.playConf
 import core.qiniu.QiniuClient
 import org.bson.types.ObjectId
+import play.api.Configuration
 import play.api.libs.json._
 import play.api.mvc.{Action, Controller}
-import play.api.{Configuration, Logger}
 
 /**
  * Created by zephyre on 4/25/15.
  */
 object MiscCtrl extends Controller {
+
+  def uploadToken = Action {
+    request => {
+      val jsonBody = request.body.asJson.get
+      val key = java.util.UUID.randomUUID.toString
+      val token = (jsonBody \ "action").asOpt[Int].get match {
+        case 1 => sendMessageToken(key = key)
+        case _ => throw new IllegalArgumentException
+      }
+
+      val result = JsObject(Seq("key" -> JsString(key), "token" -> JsString(token)))
+
+      Helpers.JsonResponse(data = Some(result))
+    }
+  }
 
   /**
    * 获得发送消息的七牛上传token
@@ -38,7 +53,7 @@ object MiscCtrl extends Controller {
 
     // 魔法变量
     val magicParams = for {
-      key <- Seq("bucket", "etag", "key", "fname", "fsize", "mimeType", "imageInfo")
+      key <- Seq("bucket", "etag", "key", "fname", "fsize", "mimeType", "imageInfo", "avinfo", "imageAve")
     } yield key -> "$(%s)".format(key)
 
     val params = urlencode(Map(customParams ++ magicParams :+ "action" -> "1": _*))
@@ -61,18 +76,21 @@ object MiscCtrl extends Controller {
     QiniuClient.uploadToken(key, expire = deadline, policy = putPolicy)
   }
 
-  def uploadToken = Action {
+  /**
+   * 处理七牛的回调。如果action为1，说明这是发送消息时的上传
+   * @return
+   */
+  def qiniuCallback() = Action.async {
     request => {
-      val jsonBody = request.body.asJson.get
-      val key = java.util.UUID.randomUUID.toString
-      val token = (jsonBody \ "action").asOpt[Int].get match {
-        case 1 => sendMessageToken(key = key)
+      val postBody = request.body.asFormUrlEncoded.get
+      // 过滤：Seq[String]不为空，并且其内容不为空字符串
+      val postMap = Map(postBody.toSeq filter ((item: (String, Seq[String])) =>
+        item._2.nonEmpty && item._2.head.nonEmpty): _*).mapValues(_.head)
+
+      postMap("action") match {
+        case "1" => qiniuSendMessageCallback(postMap)
         case _ => throw new IllegalArgumentException
       }
-
-      val result = JsObject(Seq("key" -> JsString(key), "token" -> JsString(token)))
-
-      Helpers.JsonResponse(data = Some(result))
     }
   }
 
@@ -94,10 +112,15 @@ object MiscCtrl extends Controller {
     val baseUrl = s"http://$host/$key"
     val styleSeparator = "!"
     val expire = 7 * 24 * 3600
-    Logger.info(playConf.getConfig("qiniu.style").get.entrySet.toString())
 
     val contents = msgType match {
-      case 1 => JsObject(Seq("url" -> JsString(QiniuClient.privateDownloadUrl(baseUrl, expire)))).toString()
+      case 1 =>
+        val avinfo = Json.parse(postMap.get("avinfo").get)
+        val duration = (avinfo \ "audio" \ "duration").asOpt[String].get.toDouble
+
+        JsObject(Seq(
+          "url" -> JsString(QiniuClient.privateDownloadUrl(baseUrl, expire)),
+          "duration" -> JsNumber(duration))).toString()
       case 2 =>
         val imageInfo = Json.parse(postMap.get("imageInfo").get)
 
@@ -107,7 +130,6 @@ object MiscCtrl extends Controller {
         val styleSet = for {
           (prop, value) <- playConf.getConfig("qiniu.style").get.entrySet
         } yield prop -> JsString(QiniuClient.privateDownloadUrl(buildUrlFromStyle(value.unwrapped.toString), expire))
-        Logger.info(styleSet.toString())
 
         JsObject(Seq(
           "width" -> JsNumber((imageInfo \ "width").asOpt[Int].get),
@@ -116,23 +138,5 @@ object MiscCtrl extends Controller {
       case _ => throw new IllegalArgumentException
     }
     ChatCtrl.sendMessageBase(MessageInfo(senderId, recvId, cid, msgType, Some(contents)))
-  }
-
-  /**
-   * 处理七牛的回调。如果action为1，说明这是发送消息时的上传
-   * @return
-   */
-  def qiniuCallback() = Action.async {
-    request => {
-      val postBody = request.body.asFormUrlEncoded.get
-      // 过滤：Seq[String]不为空，并且其内容不为空字符串
-      val postMap = Map(postBody.toSeq filter ((item: (String, Seq[String])) =>
-        item._2.nonEmpty && item._2(0).nonEmpty): _*).mapValues(_(0))
-
-      postMap("action") match {
-        case "1" => qiniuSendMessageCallback(postMap)
-        case _ => throw new IllegalArgumentException
-      }
-    }
   }
 }
