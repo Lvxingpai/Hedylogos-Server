7f6b7c4e8d142de159a30f19dd0463c2e90a6710
林川 <ppp21spider@sina.com>
2015-05-19 14:32:36 +0800

    MOD:接收消息添加信息

--- app/core/json/MessageFormatter.scala
+++ app/core/json/MessageFormatter.scala
@@ -9,7 +9,7 @@ import play.api.libs.json.{JsNumber, JsObject, JsString, JsValue}
 object MessageFormatter extends JsonFormatter {
   override def format(item: AbstractEntity): JsValue = {
     val msg = item.asInstanceOf[Message]
-    JsObject(Seq(
+    val stContent = Seq(
       "id" -> JsString(msg.getId.toString),
       "chatType" -> JsString(msg.getChatType),
       "msgId" -> JsNumber(msg.getMsgId.toLong),
@@ -17,32 +17,38 @@ object MessageFormatter extends JsonFormatter {
       "conversation" -> JsString(msg.getConversation.toString),
       "contents" -> JsString(msg.getContents),
       "senderId" -> JsNumber(msg.getSenderId.toLong),
-      "groupId" -> JsNumber(msg.getReceiverId.toLong),
       //      "senderAvatar" -> JsString(""),
       //      "senderName" -> JsString("测试用户"),
-      "timestamp" -> JsNumber(msg.getTimestamp.toLong)))
+      "timestamp" -> JsNumber(msg.getTimestamp.toLong))
+    val content = if (msg.getChatType.equals("group"))
+      stContent ++ Seq("groupId" -> JsNumber(msg.getReceiverId.toLong))
+    else stContent
+    JsObject(
+      content
+    )
   }
 
   def formatAddRouteKey(item: AbstractEntity, routeKey: String): JsValue = {
     val msg = item.asInstanceOf[Message]
-    JsObject(Seq(
+    val msgStContent = Seq(
+      "id" -> JsString(msg.getId.toString),
+      "chatType" -> JsString(msg.getChatType),
+      "msgId" -> JsNumber(msg.getMsgId.toLong),
+      "msgType" -> JsNumber(msg.getMsgType.toInt),
+      "conversation" -> JsString(msg.getConversation.toString),
+      "contents" -> JsString(msg.getContents),
+      "senderId" -> JsNumber(msg.getSenderId.toLong),
+      //          "senderAvatar" -> JsString(""),
+      //          "senderName" -> JsString("测试用户"),
+      "timestamp" -> JsNumber(msg.getTimestamp.toLong)
+    )
+    val msgContent = if (msg.getChatType.equals("group"))
+      msgStContent ++ Seq("groupId" -> JsNumber(msg.getReceiverId.toLong))
+    else msgStContent
+    val content = Seq(
       "routeKey" -> JsString(routeKey),
-      "message" -> JsObject(
-        Seq(
-          "id" -> JsString(msg.getId.toString),
-          "chatType" -> JsString(msg.getChatType),
-          "msgId" -> JsNumber(msg.getMsgId.toLong),
-          "msgType" -> JsNumber(msg.getMsgType.toInt),
-          "conversation" -> JsString(msg.getConversation.toString),
-          "contents" -> JsString(msg.getContents),
-          "senderId" -> JsNumber(msg.getSenderId.toLong),
-          "groupId" -> JsNumber(msg.getReceiverId.toLong),
-          //          "senderAvatar" -> JsString(""),
-          //          "senderName" -> JsString("测试用户"),
-          "timestamp" -> JsNumber(msg.getTimestamp.toLong)
-        )
-      )
+      "message" -> JsObject(msgContent)
     )
-    )
+    JsObject(content)
   }
 }
